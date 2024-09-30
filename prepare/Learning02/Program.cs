65d46fb531ab29882a630ba9603cba714fe1ef8e
lillianarp <87861783+lillianarp@users.noreply.github.com>
2024-09-30 15:47:34 +1300

    Removed extra Display() in Program.cs

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -10,7 +10,7 @@ class Program
         job1._jobTitle = "Software Engineer";
         job1._startYear = 2019;
         job1._endYear = 2022;
-        job1.Display();
+        // job1.Display();
         // Console.WriteLine(job1._company);
 
         Job job2 = new Job();
@@ -18,7 +18,7 @@ class Program
         job2._jobTitle = "Product Manager";
         job2._startYear = 2022;
         job2._endYear = 2023;
-        job2.Display();
+        // job2.Display();
         // Console.WriteLine(job2._company);
 
         Resume myResume = new Resume();
